dbc716d0d9c477a471910a201b2e393a25cb13fe
Milos Bogdanovic <bogdanovic.mlb@gmail.com>
2020-11-05 12:02:15 +0100

    code cleanup

---
+++ build/precache-manifest.fe3adbc83bf96b8f30d284e6008fe38b.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac848086ecee58d68d0c4fef224a6ab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9ba5747ef04c386ba53",
+    "url": "/static/css/2.7f4f7297.chunk.css"
+  },
+  {
+    "revision": "c427ccbdb1f53b23d9ab",
+    "url": "/static/css/main.5e13cd92.chunk.css"
+  },
+  {
+    "revision": "f9ba5747ef04c386ba53",
+    "url": "/static/js/2.82f4db2b.chunk.js"
+  },
+  {
+    "revision": "83ba115d326ad278e1950a551ea8c7b9",
+    "url": "/static/js/2.82f4db2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c427ccbdb1f53b23d9ab",
+    "url": "/static/js/main.c4102070.chunk.js"
+  },
+  {
+    "revision": "966341e26f85329e8c1c",
+    "url": "/static/js/runtime-main.a1e0bcfb.js"
+  },
+  {
+    "revision": "1c407dfbe95e88c2fd06cca6a5b98730",
+    "url": "/static/media/C-doctor_photo.1c407dfb.jpg"
+  },
+  {
+    "revision": "94bb76db08f9cd1464454290501fcc7b",
+    "url": "/static/media/Extend-screen.94bb76db.svg"
+  },
+  {
+    "revision": "e2ec758bb05a38faf8d1332345fc9d33",
+    "url": "/static/media/Make-a-video.e2ec758b.svg"
+  },
+  {
+    "revision": "c5526b7224a77899e590ae47090b7da2",
+    "url": "/static/media/My_Patients_blue.c5526b72.svg"
+  },
+  {
+    "revision": "779c6df653542687a1c34fbd107703e1",
+    "url": "/static/media/My_Patients_white.779c6df6.svg"
+  },
+  {
+    "revision": "068d2c829e9a8c2d3ea8406543724727",
+    "url": "/static/media/Request-email.068d2c82.svg"
+  },
+  {
+    "revision": "3af5ffee77485d9c0963aad6dd76ddbf",
+    "url": "/static/media/arrow-left.3af5ffee.svg"
+  },
+  {
+    "revision": "010e4ba35376530e01a946881fc47afa",
+    "url": "/static/media/arrow-right.010e4ba3.svg"
+  },
+  {
+    "revision": "5d226fb5004d80ae5452d99b4e5d9b27",
+    "url": "/static/media/arrow_down_gray.5d226fb5.svg"
+  },
+  {
+    "revision": "45f3b473bc4fd395571e2b044b1755b4",
+    "url": "/static/media/arrow_up_gray.45f3b473.svg"
+  },
+  {
+    "revision": "f61deb9e5e20b328ec160a85c4912a43",
+    "url": "/static/media/attach_white.f61deb9e.svg"
+  },
+  {
+    "revision": "dc2b029864b4d43a3ed4f197401932f4",
+    "url": "/static/media/burger.dc2b0298.svg"
+  },
+  {
+    "revision": "d28371ae36a7a815ea2e549bf67bdc2a",
+    "url": "/static/media/c+.d28371ae.svg"
+  },
+  {
+    "revision": "b84e16881434f008921564f675281b05",
+    "url": "/static/media/camera-off.b84e1688.svg"
+  },
+  {
+    "revision": "8538bb5bc9a5eabb8b2dba950f3babdd",
+    "url": "/static/media/camera-on.8538bb5b.svg"
+  },
+  {
+    "revision": "9dc81cc07b7be0567a5296125f41f13b",
+    "url": "/static/media/chek.9dc81cc0.svg"
+  },
+  {
+    "revision": "deadebb6d6aa169b240ae8b3cd406984",
+    "url": "/static/media/enter-waiting-room.deadebb6.svg"
+  },
+  {
+    "revision": "6d9b16741f8c5361086d67c0600a2042",
+    "url": "/static/media/hang-up.6d9b1674.svg"
+  },
+  {
+    "revision": "849a5cd24b64abe0d04e35740ba18e36",
+    "url": "/static/media/icon_Consult-with-doctor.849a5cd2.svg"
+  },
+  {
+    "revision": "2215b8e5bf825f4ca236c4726ef16e76",
+    "url": "/static/media/icon_Dashboard.2215b8e5.svg"
+  },
+  {
+    "revision": "5756618deb90a14812eadd57e0e8fde3",
+    "url": "/static/media/icon_Email_blue.5756618d.svg"
+  },
+  {
+    "revision": "3d6339e963a05b2e0f72bf3da77665f4",
+    "url": "/static/media/icon_Log_Out_blue.3d6339e9.svg"
+  },
+  {
+    "revision": "6e1e3769faffc66b8c909762b7dd7cae",
+    "url": "/static/media/icon_My_Consultations_blue.6e1e3769.svg"
+  },
+  {
+    "revision": "8637fce839140a8d3cf44e82ed70d93c",
+    "url": "/static/media/icon_Payment.8637fce8.svg"
+  },
+  {
+    "revision": "9c498defa9c756289edee38a2fdf5616",
+    "url": "/static/media/icon_Request-consultation.9c498def.svg"
+  },
+  {
+    "revision": "55d8376de0e62ffbb2061d0298351fbb",
+    "url": "/static/media/icon_Video_Appointment_blue.55d8376d.svg"
+  },
+  {
+    "revision": "a97bcac54305a591b51725fcd47d9d25",
+    "url": "/static/media/icon_Waiting_Room_blue.a97bcac5.svg"
+  },
+  {
+    "revision": "d12b9584bed6c23dbfd78113da8adb2e",
+    "url": "/static/media/icon_alerts_white.d12b9584.svg"
+  },
+  {
+    "revision": "de474e09e80d050da6fbcc37a0298577",
+    "url": "/static/media/icon_calendar_white.de474e09.svg"
+  },
+  {
+    "revision": "25624e1c5b7984d7f42d45b18d180f27",
+    "url": "/static/media/icon_doctor_2_blue.25624e1c.svg"
+  },
+  {
+    "revision": "c6d033191d7b9dc432a053a326e93ec4",
+    "url": "/static/media/icon_doctor_2_white.c6d03319.svg"
+  },
+  {
+    "revision": "da9cdbcaecfbaa4c22bb76a1e58e9f8f",
+    "url": "/static/media/icon_my_profile_client_blue_23px.da9cdbca.svg"
+  },
+  {
+    "revision": "3f75bc403b171022c6d9b60e2c362ec2",
+    "url": "/static/media/icon_my_profile_client_on-line_46px.3f75bc40.svg"
+  },
+  {
+    "revision": "64c488b4f3e077e1205fe12677d61463",
+    "url": "/static/media/icon_my_profile_client_white_23px.64c488b4.svg"
+  },
+  {
+    "revision": "c8fc88ecc53f3c954832c1c5c6cf74eb",
+    "url": "/static/media/icon_my_profile_doctor_blue_23px.c8fc88ec.svg"
+  },
+  {
+    "revision": "6b7c020b2533ff367fbc78fc3b50db94",
+    "url": "/static/media/icon_my_profile_doctor_off-line_46px.6b7c020b.svg"
+  },
+  {
+    "revision": "f625c17e55e9d158b813a89d81fb43fd",
+    "url": "/static/media/icon_my_profile_doctor_on-line_46px.f625c17e.svg"
+  },
+  {
+    "revision": "c2a084f27fa6977605f97201bb5b5e14",
+    "url": "/static/media/icon_settings_blue.c2a084f2.svg"
+  },
+  {
+    "revision": "9eb912d5fe79c7a1865111e1a31a8598",
+    "url": "/static/media/icon_settings_white.9eb912d5.svg"
+  },
+  {
+    "revision": "b3d89f609e05f2209d6973c8bb220f08",
+    "url": "/static/media/icon_sign-up.b3d89f60.svg"
+  },
+  {
+    "revision": "bd28d0e72c703223e16493c73d94e196",
+    "url": "/static/media/logo.bd28d0e7.svg"
+  },
+  {
+    "revision": "c267d2ff70858f61a4feadf85f7d9248",
+    "url": "/static/media/mute.c267d2ff.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "3b0370493d55de37457a240682df94f4",
+    "url": "/static/media/password-hidden.3b037049.svg"
+  },
+  {
+    "revision": "0ae822f13002d1dd7a95ae9166a68b05",
+    "url": "/static/media/password-seen.0ae822f1.svg"
+  },
+  {
+    "revision": "67f266f448688bf51befa1bdf6951320",
+    "url": "/static/media/unmute.67f266f4.svg"
+  }
+]);
